8c9bc27d97e0dd02a7c6f2a2a81f1ba35f2e5eff
Simon Demele <simon.demele@finet.de>
2018-08-16 17:26:08 +0200

    Adapt Class Genration

--- src/Php/ClassGenerator.php
+++ src/Php/ClassGenerator.php
@@ -5,13 +5,10 @@ use GoetasWebservices\Xsd\XsdToPhp\Php\Structure\PHPClass;
 use GoetasWebservices\Xsd\XsdToPhp\Php\Structure\PHPClassOf;
 use GoetasWebservices\Xsd\XsdToPhp\Php\Structure\PHPProperty;
 use Zend\Code\Generator;
-use Zend\Code\Generator\DocBlock\Tag\ParamTag;
 use Zend\Code\Generator\DocBlock\Tag\GenericTag;
 use Zend\Code\Generator\DocBlock\Tag\PropertyTag;
-use Zend\Code\Generator\DocBlock\Tag\ReturnTag;
 use Zend\Code\Generator\DocBlockGenerator;
 use Zend\Code\Generator\MethodGenerator;
-use Zend\Code\Generator\ParameterGenerator;
 use Zend\Code\Generator\PropertyGenerator;
 
 class ClassGenerator
@@ -28,79 +25,21 @@ class ClassGenerator
 
         $class->addConstant('PROPERTY_MAP', $propertyMap);
 
+        $method = new MethodGenerator("getPropertyMap", []);
+        if ($class->getExtendedClass()) {
+            $methodBody = "\$properties = parent::getPropertyMap();" . PHP_EOL;
+        } else {
+            $methodBody = "\$properties = [];" . PHP_EOL;
+        }
+        $methodBody .= "return array_merge(\$properties, static::PROPERTY_MAP);" . PHP_EOL;
+        $method->setBody($methodBody);
+        $class->addMethodFromGenerator($method);
+
         if (count($type->getProperties()) === 1 && $type->hasProperty('__value')) {
             return false;
         }
 
         return true;
-    }
-
-    private function handleValueMethod(Generator\ClassGenerator $generator, PHPProperty $prop, PHPClass $class, $all = true)
-    {
-        $type = $prop->getType();
-
-        $docblock = new DocBlockGenerator('Construct');
-        $docblock->setWordWrap(false);
-        $paramTag = new ParamTag("value");
-        $paramTag->setTypes(($type ? $type->getPhpType() : "mixed"));
-
-        $docblock->setTag($paramTag);
-
-        $param = new ParameterGenerator("value");
-        if ($type && !$type->isNativeType()) {
-            $param->setType($type->getPhpType());
-        }
-        $method = new MethodGenerator("__construct", [
-            $param
-        ]);
-        $method->setDocBlock($docblock);
-        $method->setBody("\$this->value(\$value);");
-
-        $generator->addMethodFromGenerator($method);
-
-        $docblock = new DocBlockGenerator('Gets or sets the inner value');
-        $docblock->setWordWrap(false);
-        $paramTag = new ParamTag("value");
-        if ($type && $type instanceof PHPClassOf) {
-            $paramTag->setTypes($type->getArg()->getType()->getPhpType() . "[]");
-        } elseif ($type) {
-            $paramTag->setTypes($prop->getType()->getPhpType());
-        }
-        $docblock->setTag($paramTag);
-
-        $returnTag = new ReturnTag("mixed");
-
-        if ($type && $type instanceof PHPClassOf) {
-            $returnTag->setTypes($type->getArg()->getType()->getPhpType() . "[]");
-        } elseif ($type) {
-            $returnTag->setTypes($type->getPhpType());
-        }
-        $docblock->setTag($returnTag);
-
-        $param = new ParameterGenerator("value");
-        $param->setDefaultValue(null);
-
-        if ($type && !$type->isNativeType()) {
-            $param->setType($type->getPhpType());
-        }
-        $method = new MethodGenerator("value", []);
-        $method->setDocBlock($docblock);
-
-        $methodBody = "if (\$args = func_get_args()) {" . PHP_EOL;
-        $methodBody .= "    \$this->" . $prop->getName() . " = \$args[0];" . PHP_EOL;
-        $methodBody .= "}" . PHP_EOL;
-        $methodBody .= "return \$this->" . $prop->getName() . ";" . PHP_EOL;
-        $method->setBody($methodBody);
-
-        $generator->addMethodFromGenerator($method);
-
-        $docblock = new DocBlockGenerator('Gets a string value');
-        $docblock->setWordWrap(false);
-        $docblock->setTag(new ReturnTag("string"));
-        $method = new MethodGenerator("__toString");
-        $method->setDocBlock($docblock);
-        $method->setBody("return strval(\$this->" . $prop->getName() . ");");
-        $generator->addMethodFromGenerator($method);
     }
 
     private function handleProperty(Generator\ClassGenerator $class, PHPProperty $prop)
